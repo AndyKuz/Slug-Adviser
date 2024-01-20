b87ab5f0758f0c5793ed6b7f6f885fdeabf3a8a5
Aravind Nair <anair9@ucsc.edu>
2024-01-20 13:39:20 -0800

    more updates to restructure of remote preprocess branch

--- Pre-Process/Professor.py
+++
@@ -1,9 +0,0 @@
-class Professor:
-    def __init__(self, professorName, difficultyLevel, professorRating):
-        self.professorName = professorName
-        self.difficultyLevel = difficultyLevel
-        self.professorRating = professorRating
-
-
-        
-    
