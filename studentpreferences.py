1c1e5dd051505c3a5caea7784cb025d49c9d37fd
Aravind Nair <anair9@ucsc.edu>
2024-01-20 12:43:24 -0800

    placing student preferences class into mainline

---
+++ studentpreferences.py
@@ -0,0 +1,7 @@
+class StudentPreferences:
+    def __init__(self, minHoursWork, maxHoursWork, minCreditsQuarter, maxCreditsQuarter, summerClasses):
+        self.minHoursWork = minHoursWork
+        self.maxHoursWork = maxHoursWork
+        self.minCreditsQuarter = minCreditsQuarter
+        self.maxCreditsQuarter = maxCreditsQuarter
+        self.summerClasses = summerClasses
