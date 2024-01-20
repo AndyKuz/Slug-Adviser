cac90c2383e0b381f7fb08883ee9704714e6da3d
Andrew Kuznetsov <andrew.kuz137@gmail.com>
2024-01-20 01:32:57 -0800

    Staashing changes and moving Course class

--- algorithm/digraph.py
+++ algorithm/digraph.py
@@ -22,9 +22,35 @@ class Digraph:
             c1.outgoing_prereqs.append(c2)
         if c1 not in c2.incoming_prereqs:
             c2.incoming_prereqs.append(c1)
-    
-    def del_edge(self, c1, c2):
-        pass
 
-    def bfs(self, c):
-        pass
+    def del_edge(self, c1, c2):
+        c1.outgoing_prereqs.remove(c2)
+        c2.incoming_prereqs.remove(c1)
+
+    def del_course(self, c1):
+        if c1 in self.root_courses:
+            self.root_courses.remove(c1)
+        if c1 in self.all_courses:
+            self.all_courses.remove(c1)
+
+    def choose_quarter(self, num_classes):
+        chosen_classes = []
+        root_start = 0  # which root node we should iterate from
+        if self.root_courses:
+            chosen_classes = [self.root_courses[root_start]]
+            if len(self.root_courses) > root_start:
+                root_start+=1
+        
+        visited = []    # list of visited classes
+        queue = []      # list of classes to visit
+
+        queue.append(self.root_courses[root_start])
+        
+        while len(chosen_classes) < num_classes or not queue:
+            pass
+        
+
+            
+
+
+
--- algorithm/schedule_maker.py
+++ algorithm/schedule_maker.py
@@ -1,17 +1,5 @@
 from digraph import Digraph
 
-class Course:
-    def __init__(self, dptmnt, dptmnt_num, course_title, num_credits, incoming_prereqs, score=2.5, outgoing_prereqs=None, quarters=None, profs=None):
-        self.dptmnt = dptmnt
-        self.dptmnt_num = dptmnt_num
-        self.course_title = course_title
-        self.num_credits = num_credits
-        self.score = score
-        self.incoming_prereqs = incoming_prereqs
-        self.outgoing_prereqs = outgoing_prereqs if outgoing_prereqs is not None else []
-        self.quarters = quarters if quarters is not None else []
-        self.profs = profs if profs is not None else []
-
 def create_digraph(courses_list):
     dg = Digraph() 
     for c in courses_list:
