e4c9d38c67d9cee2c795589c700f9eb81958090f
Chad Baker <chadster5200@gmail.com>
2024-01-20 17:09:10 -0800

    added classes

--- courseInfo.py
+++ courseInfo.py
@@ -20,11 +20,16 @@ cse_102 = course.Course(dptmnt='CSE', dptmnt_num='102', course_title='Introducti
 cse_103 = course.Course(dptmnt='CSE', dptmnt_num='103', course_title='Computational Models', num_credits=5, incoming_prereqs=[cse_101], quarters=[], profs=[])
 cse_107 = course.Course(dptmnt='CSE', dptmnt_num='107', course_title='Probability and Statistics for Engineers', num_credits=5, incoming_prereqs=[cse_16, am_30], quarters=[], profs=[])
 cse_120 = course.Course(dptmnt='CSE', dptmnt_num='120', course_title='Computer Architecture', num_credits=5, incoming_prereqs=[cse_12, cse_13s], quarters=[], profs=[])
+cse_125 = course.Course(dptmnt='CSE', dptmnt_num='125', course_title='Logic Design with Verilog', num_credits=7, incoming_prereqs=[cse_100, cse_120], quarters=[], profs=[])
 cse_130 = course.Course(dptmnt='CSE', dptmnt_num='130', course_title='Principles of Computer Systems Design', num_credits=5, incoming_prereqs=[cse_12, cse_101], quarters=[], profs=[])
+cse_150 = course.Course(dptmnt='CSE', dptmnt_num='150', course_title='Introduction to Computer Networks', num_credits=7, incoming_prereqs=[cse_12, cse_16, cse_30], quarters=[], profs=[])
 cse_185e = course.Course(dptmnt='CSE', dptmnt_num='185E', course_title='Technical Writing for Computer Science and Engineering', num_credits=5, incoming_prereqs=[cse_12], quarters=[], profs=[])
 ece_30 = course.Course(dptmnt='ECE', dptmnt_num='30', course_title='Engineering Principles of Electronics', num_credits=5, incoming_prereqs=[math_19b], quarters=[], profs=[])
 ece_101 = course.Course(dptmnt='ECE', dptmnt_num='101/L', course_title='Introduction to Electronic Circuits', num_credits=7, incoming_prereqs=[phys_5c, am_20], quarters=[], profs=[])
+cse_111 = course.Course(dptmnt='CSE', dptmnt_num='111', course_title='Advanced Programming', num_credits=5, incoming_prereqs=[cse_13s], quarters=[], profs=[])
 cse_121 = course.Course(dptmnt='CSE', dptmnt_num='121', course_title='Embedded System Design', num_credits=7, incoming_prereqs=[cse_12, cse_100, cse_13s, ece_101, phys_5c], quarters=[], profs=[])
+cse_122 = course.Course(dptmnt='CSE', dptmnt_num='122', course_title='Introduction to VLSI Digital System Design', num_credits=5, incoming_prereqs=[cse_100, ece_101], quarters=[], profs=[])
+cse_220 = course.Course(dptmnt='CSE', dptmnt_num='220', course_title='Computer Architecture', num_credits=5, incoming_prereqs=[cse_120], quarters=[], profs=[])
 ece_103 = course.Course(dptmnt='ECE', dptmnt_num='103/L', course_title='Signals and Systems', num_credits=7, incoming_prereqs=[ece_101, am_20], quarters=[], profs=[])
 cse_112orcse_114a = course.Course(dptmnt='CSE', dptmnt_num='112 or 114A', course_title='Comparative Programming Languages', num_credits=5, incoming_prereqs=[cse_101], quarters=[], profs=[])
 stat_131orcse_107 = course.Course(dptmnt='STAT or CSE', dptmnt_num='131 or 107', course_title='Introduction to Probability Theory', num_credits=5, incoming_prereqs=[math_19b], quarters=[], profs=[])
@@ -32,6 +37,11 @@ stat_131orcse_107 = course.Course(dptmnt='STAT or CSE', dptmnt_num='131 or 107',
 CE = [math_19a, math_19b, am_10, am_20, am_30, cse_20, cse_30, cse_12, cse_13s, cse_16,phys_5a, 
       phys_5c, phys_5b, cse_100, cse_101, cse_107, cse_120, cse_185e, ece_101, cse_121, ece_103]
 
-CS = [cse_12, cse_16, cse_20, cse_30, cse_13s, math_19a, math_19b, am_10, am_30, ece_30, cse_101, cse_102, cse_103, cse_120, cse_130, cse_112orcse_114a, stat_131orcse_107]
+CS = [cse_12, cse_16, cse_20, cse_30, cse_13s, math_19a, math_19b, am_10, am_30, ece_30, cse_101, 
+      cse_102, cse_103, cse_120, cse_130, cse_112orcse_114a, stat_131orcse_107]
 
-schedule_maker.create_schedule(CS, [cse_20, cse_30, math_19a, math_19b], 1)
+Digital_Hardware = [cse_125, cse_122, cse_220]
+
+CE.extend(Digital_Hardware)
+
+schedule_maker.create_schedule(CE, [cse_20, cse_30, math_19a, math_19b], 1)
